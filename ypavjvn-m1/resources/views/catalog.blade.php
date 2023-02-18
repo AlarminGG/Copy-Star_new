8c004ef628aaf9b94a174293d7ca24a4fa3ef401
AlarminGG <jojooriginal@mail.ru>
2023-02-18 02:59:20 -0800

    18.02

--- ypavjvn-m1/resources/views/catalog.blade.php
+++ ypavjvn-m1/resources/views/catalog.blade.php
@@ -1,38 +1,43 @@
 @extends('layouts.app')
 
-    @section('content')
+@section('content')
     <div class="catalog">
         <div class="container">
             <h1>Каталог</h1>
             <form action="/public/catalog">
-                <select name="filter" id="filter"></select>
-                <option value="created_at">Новизна</option>
-                <option value="price">Цена</option>
-                <option value="name">Название</option>
-                <option value="year">Год</option>
-                <select name="category" id="category">
-                    @foreach($category as $c)
-                        <option value="{{$c->id}}">{{$c->name}}></option> 
-                    @endforeach
-                </select>
-                <button type="submit">Применить</button>
+                <select name="filter" id="filter">
+                    <option value="created_at">Новизна</option>
+                    <option value="price">Цена</option>
+                    <option value="name">Название</option>
+                    <option value="year">Год</option>
+                    <select name="category" id="category">
+                        @foreach ($category as $c)
+                            <option value="{{ $c->id }}">{{ $c->name }}></option>
+                        @endforeach
+                    </select>
+                    <button type="submit">Применить</button>
             </form>
             <div class="all-tovar">
-            @foreach($products as $p)
-            <div class="tovar">
-                <div class="tovar-img">
-                    <a href="/public/card/{{$p->id}}"><img src="/public/img/{{$p->photo}}" alt="..." class="img-fluid"></a>
-                </div>
-                <div class="tovar-item">
-                    <div class="tovar-name"><p>{{$p->name}}</p></div>
-                    <div class="tovar-item">
-                        <p>{{$p->price}} ₽</p>
-                        @guest @else <a href="/public/product/{{$p->id}}">купить</a> @endguest
+                @foreach ($products as $p)
+                    <div class="tovar">
+                        <div class="tovar-img">
+                            <a href="/public/card/{{ $p->id }}"><img src="/public/img/{{ $p->photo }}"
+                                    alt="..." class="img-fluid"></a>
+                        </div>
+                        <div class="tovar-item">
+                            <div class="tovar-name">
+                                <p>{{ $p->name }}</p>
+                            </div>
+                            <div class="tovar-item">
+                                <p>{{ $p->price }} ₽</p>
+                                @guest
+                                @else
+                                <a href="/public/product/{{ $p->id }}">купить</a> @endguest
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            @endforeach
+                @endforeach
             </div>
         </div>
     </div>
-    @endsection
+@endsection
--- ypavjvn-m1/resources/views/welcome.blade.php
+++ ypavjvn-m1/resources/views/welcome.blade.php
@@ -1,43 +1,45 @@
     @extends('layouts.app')
 
     @section('content')
-    <div class="welcome">
-        <div class="container">
-            <div class="info">
-                <div class="info-img"><img src="/public/img/logo.png"></div>
-                <div class="info-text">
-                    <p>О нас</p>
-                    <h1>Шоппинг в век технологий.</h1>
+        <div class="welcome">
+            <div class="container d-flex">
+                <div class="info">
+                    <div class="info-img"><img src="/public/img/logo.png"></div>
+                    <div class="info-text">
+                        <p>О нас</p>
+                        <h1>Доставляя удовольствие от интеллектуальной печати.</h1>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="container">
+                <div class="row">
+                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="/public/img/tovar1.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="/public/img/tovar2.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="/public/img/tovar3.jpg" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Назад</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Вперед</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-
-
-
-        <div class="container">
-            <div class="row">
-                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                        <img src="/public/img/tovar1.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                        <img src="/public/img/tovar2.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                        <img src="/public/img/tovar3.jpg" class="d-block w-100" alt="...">
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Назад</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Вперед</span>
-                    </button>
-                    </div>
-            </div>
-        </div>
-    </div>
     @endsection
